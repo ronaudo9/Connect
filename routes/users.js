3ff8299584a94fe786d15cf3a0f19a905e95a636
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2023-03-30 16:34:09 +0900

    いいね機能・タイムライン　バックエンドAPI実装完了

--- routes/users.js
+++ routes/users.js
@@ -40,17 +40,80 @@ router.delete("/:id", async (req, res) => {
 
 //ユーザー情報の取得
 router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    //user._docはユーザーの中にあるすべての情報_docはMongoDBで配列を取り出す際に使用される
+    //...otherは分割代入
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
-      //user._docはユーザーの中にあるすべての情報_docはMongoDBで配列を取り出す際に使用される
-      //...otherは分割代入
-      const {password,updatedAt,...other} = user._doc;
-      res.status(200).json(other);
+      const currentUser = await User.findById(req.body.userId);
+      //フォロワーに自分がいなかったらフォローできる
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォローに成功しました！");
+      } else {
+        return res
+          .status(403)
+          .json("あなたはすでにこのユーザーをフォローしています。");
+      }
     } catch (err) {
       return res.status(500).json(err);
     }
+  } else {
+    return res.status(500).json("自分自身をフォローできません。");
+  }
 });
 
+//ユーザーのフォローを外す
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      //フォロワーに存在したらフォローを外せる
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $pull: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォロー解除しました！");
+      } else {
+        return res
+          .status(403)
+          .json("このユーザーはフォロー解除できません。");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("自分自身をフォロー解除できません。");
+  }
+});
 
 // roter.get("/", (req, res) => {
 //   res.send("user router");
